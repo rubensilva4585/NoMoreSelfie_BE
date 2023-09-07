211a847cae7ba6071bea4456bbba71e01e569624
DSoares <david.soares.t0123935@edu.atec.pt>
2023-09-07 22:51:20 +0100

    ID001 [BE] - Seeder Category + SubCategory

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class CategorySeeder extends Seeder
 {
@@ -12,6 +13,28 @@ class CategorySeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $categories = [
+            'Casamentos',
+            'Bebés',
+            'Edição',
+            'Drone',
+            'Festas',
+            'Eventos Empresariais',
+            'Restauração',
+            'Turismo',
+            'Moda',
+            'Lazer',
+            'Imoveis',
+            'Desporto',
+        ];
+
+        foreach ($categories as $category) {
+            DB::table('categories')->insert([
+                'name' => $category,
+                'inPerson' => true,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
+        }
     }
 }
--- database/seeders/SubCategorySeeder.php
+++ database/seeders/SubCategorySeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class SubCategorySeeder extends Seeder
 {
@@ -12,6 +13,92 @@ class SubCategorySeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $categoryMappings = [
+            'Casamentos' => [
+                'Pedido de Casamento',
+                'Pré Casamento',
+                'Pós Csamento',
+                'Casamento',
+            ],
+            'Bebés' => [
+                'Sessão Newborn',
+                'Ensaio de Gravidez',
+                'Álbum de Bebê',
+                'Batizado',
+            ],
+            'Edição' => [
+                'Edição de Vídeo',
+                'Edição de Fotografia',
+                'Pós-Produção',
+                'Efeitos Especiais',
+            ],
+            'Drone' => [
+                'Fotografia Aérea',
+                'Vídeo Aéreo',
+                'Mapeamento com Drone',
+                'Inspeção com Drone',
+            ],
+            'Festas' => [
+                'Aniversário',
+                'Temáticas',
+                'Despedida de Solteiro(a)',
+                'Natal',
+                'Ano Novo',
+            ],
+            'Eventos Empresariais' => [
+                'Conferências',
+                'Seminários',
+                'Lançamentos de Produtos',
+                'Eventos Corporativos',
+                'Feiras Comerciais',
+            ],
+            'Restauração' => [
+                'Fotografia de Alimentos',
+                'Fotografia de Ambiente',
+                'Cardápio',
+                'Vídeo Promocional de Restaurante',
+                'Chef em Ação',
+            ],
+            'Turismo' => [
+                'Fotografia de Destinos',
+                'Vídeo de Viagens',
+                'Visitas Guiadas',
+            ],
+            'Moda' => [
+                'Ensaio de Moda',
+                'Desfiles de Moda',
+                'Fotografia de Catálogos',
+                'Editoriais de Moda',
+            ],
+            'Lazer' => [
+                'Shows e Concertos',
+                'Parques de Diversões',
+                'Viagens de Lazer',
+            ],
+            'Imóveis' => [
+                'Fotografia de Imóveis',
+                'Vídeo de Imóveis',
+                'Tour Virtual',
+            ],
+            'Desporto' => [
+                'Cobertura de Eventos desportivos',
+                'Retratos de Atletas',
+                'Vídeo de Treino',
+                'Desportos Radicais',
+            ],
+        ];        
+
+        foreach ($categoryMappings as $categoryName => $subcategories) {
+            $categoryId = DB::table('categories')->where('name', $categoryName)->value('id');
+
+            foreach ($subcategories as $subcategory) {
+                DB::table('sub_categories')->insert([
+                    'id_category' => $categoryId,
+                    'name' => $subcategory,
+                    'created_at' => now(),
+                    'updated_at' => now(),
+                ]);
+            }
+        }
     }
 }
