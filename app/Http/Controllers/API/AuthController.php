f2d061bfce82610aec331129341aa8f7cf3ddf89
Ruben <Ruben.Silva.T0123944@edu.atec.pt>
2023-10-18 20:15:10 +0100

    #ID015 - Autenticação

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\API;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Schema;
-use Illuminate\Support\Facades\Schema;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
