1c0bcb89feab41c8f1ce138a5e3d3f1473c2d56c
Rui Teixeira <rui.teixeira.t0123945@edu.atec.pt>
2023-10-20 18:06:40 +0100

    small fix

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -10,17 +10,18 @@ use App\Http\Controllers\Controller;
 
 class UserController extends Controller
 {
-    public function updateUser(Request $request) {
+    public function updateUser(Request $request)
+    {
 
         $user = Auth::user();
 
         $user->update($request->only(['name']));
-        $user->profile()->update($request->only(['phone', 'company', 'nif', 'address', 'bio']));
-        return response()->json(['data' => Auth::user(), 'message' => 'Update successfull'], 200);        
-    
+        $user->profile()->update($request->only(['phone', 'company', 'nif', 'dob', 'address', 'bio']));
+        return response()->json(['data' => Auth::user(), 'message' => 'Update successfull'], 200);
     }
 
-    public function changePassword(Request $request) {
+    public function changePassword(Request $request)
+    {
 
         $user = Auth::user();
 
@@ -28,9 +29,9 @@ class UserController extends Controller
             'oldpassword' => 'required|string|min:6',
             'newpassword' => 'required|string|min:6',
         ]);
-    
+
         if (!Hash::check($request->input('oldpassword'), $user->password)) {
-            return response()->json(['error' => 'Invalid password'], 401);   
+            return response()->json(['error' => 'Invalid password'], 401);
         }
 
         if (Hash::check($request->newpassword, $user->password)) {
