47241363755fe77c248e386f988575ac107a924b
Rui Teixeira <rui.teixeira.t0123945@edu.atec.pt>
2023-10-28 18:45:58 +0100

    added admin default account

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
 
-        //$this->call(UserSeeder::class);
+        $this->call(UserSeeder::class);
         $this->call(DistrictSeeder::class);
         //$this->call(ProfileSeeder::class);
         $this->call(CategorySeeder::class);
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\User;
 use App\Models\Profile;
+use DB;
 
 class UserSeeder extends Seeder
 {
@@ -14,6 +15,17 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory()->count(50)->create();
+        DB::table('users')->insert([
+            'name' => 'Admin',
+            'email' => 'admin@admin.com',
+            'role' => 'admin',
+            'password' => bcrypt('password'),
+            'email_verified_at' => now(),
+            'phone' => '123456789',
+            'dob' => now(),
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        // User::factory()->count(50)->create();
     }
 }
